dc023c9c3a6ad840b793f8f033a5237819a37655
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-18 21:28:16 +0700

    add class Fibonacci

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("Find the number in the Fibonacci range");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an index in the Fibonacci range");
+        int index = scanner.nextInt();
+
+        int result = findValue(index);
+        System.out.println("The value at position " + index + " in the Fibonacci range is " + result);
+    }
+    private static int findValue(int index) {
+        if(index<3) return 1;
+
+        return findValue(index - 2) + findValue(index - 1);
+
+
+    }
+}
